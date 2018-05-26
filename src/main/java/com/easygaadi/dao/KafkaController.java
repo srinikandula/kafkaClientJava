2d18db0f14e359dc7b6537c3790abc88a227c411
srinikandula <srini.kandula@shodogg.com>
2018-05-26 14:43:45 +0530

    request params

--- src/main/java/com/easygaadi/dao/KafkaController.java
+++ src/main/java/com/easygaadi/dao/KafkaController.java
@@ -53,6 +53,7 @@ final class KafkaController {
         Map<String, String[]> requestParams = request.getParameterMap();
         LOGGER.info("GET: request params ", requestParams.toString());
         LOGGER.info("GET: request param names ", request.getParameterNames());
+        LOGGER.info("GET: latitude ", latitude);
         BasicDBObject position = new BasicDBObject();
        // sender.send(position.toString());
         //LOGGER.info("sending", position.toString());

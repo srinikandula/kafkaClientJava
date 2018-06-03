e9129a4e97ab5ac6d7720fa54ef37b398f40af19
srinikandula <srini.kandula@shodogg.com>
2018-06-03 13:14:08 +0530

    Changing the stop logic

--- src/main/java/com/easygaadi/kafka/consumer/Receiver.java
+++ src/main/java/com/easygaadi/kafka/consumer/Receiver.java
@@ -13,7 +13,6 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
-import java.awt.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -93,35 +92,28 @@ public final class Receiver {
                     DevicePosition lastLocation = objectMapper.readValue(objectMapper.writeValueAsString(object), DevicePosition.class);
                     if (lastLocation.getLocation() != null) {
                         List<Double> lastCoordinates = (List<Double>)((Map)lastLocation.getLocation()).get("coordinates");
-                        if (lastCoordinates.get(0) == currentLocation.getLongitude() &&
-                                lastCoordinates.get(1) == currentLocation.getLatitude()) {
-                            LOG.info("Same as old location");
+                        if (currentLocation.getSpeed() == 0) {
+                            LOG.info("speed is zero : STOPPED!!");
                             // if the current location is same as the last location and is already marked as 'Stopped' do not save it in to the database
-
-                            if(lastLocation.isIsStopped()){
+                            currentLocation.setIdle(true);
+                            currentLocation.setStopped(true);
+                            if(lastLocation.isStopped()) {
+                                LOG.info("speed is zero and not moved : Skpping");
                                 return;
                             }
-                            if (System.currentTimeMillis() - lastLocation.getCreatedAt().getMillis() > stopTime) {
-                                currentLocation.setIsIdle(true);
-                                currentLocation.setIsStopped(true);
-                            } else {
-                                currentLocation.setIsIdle(lastLocation.isIsIdle());
-                                currentLocation.setIsStopped(lastLocation.isIsStopped());
-                            }
-                            currentLocation.setDistance(0);
-                            currentLocation.setTotalDistance(lastLocation.getTotalDistance());
-                        } else { //calculate the distance travelled
-                            currentLocation.setIsIdle(false);
-                            currentLocation.setIsStopped(false);
-                            double lastLongitude = lastCoordinates.get(0);
-                            double lastLatitude = lastCoordinates.get(1);
-                            double currentLatitude = currentLocation.getLatitude();
-                            double currentLongitude = currentLocation.getLongitude();
-                            //position.distance = 1.609344 * 3956 * 2 * Math.asin(Math.sqrt(Math.pow(Math.sin((latitude-position.location.coordinates[1])*Math.PI/180 /2),2)+Math.cos(latitude*Math.PI/180)*Math.cos(position.location.coordinates[1]*Math.PI/180)*Math.pow(Math.sin((longitude-position.location.coordinates[0])*Math.PI/180/2),2)))
-                            double distance = 1.609344 * 3956 * 2 * Math.asin(Math.sqrt(Math.pow(Math.sin((currentLatitude - lastLatitude) * Math.PI / 180 / 2), 2) + Math.cos(lastLatitude * Math.PI / 180) * Math.cos(currentLatitude * Math.PI / 180) * Math.pow(Math.sin((currentLongitude - lastLongitude) * Math.PI / 180 / 2), 2)));
-                            currentLocation.setDistance(distance);
-                            currentLocation.setTotalDistance(lastLocation.getTotalDistance() + distance);
+                        } else {
+                            currentLocation.setIdle(false);
+                            currentLocation.setStopped(false);
                         }
+                        //calculate the distance travelled
+                        double lastLongitude = lastCoordinates.get(0);
+                        double lastLatitude = lastCoordinates.get(1);
+                        double currentLatitude = currentLocation.getLatitude();
+                        double currentLongitude = currentLocation.getLongitude();
+                        //position.distance = 1.609344 * 3956 * 2 * Math.asin(Math.sqrt(Math.pow(Math.sin((latitude-position.location.coordinates[1])*Math.PI/180 /2),2)+Math.cos(latitude*Math.PI/180)*Math.cos(position.location.coordinates[1]*Math.PI/180)*Math.pow(Math.sin((longitude-position.location.coordinates[0])*Math.PI/180/2),2)))
+                        double distance = 1.609344 * 3956 * 2 * Math.asin(Math.sqrt(Math.pow(Math.sin((currentLatitude - lastLatitude) * Math.PI / 180 / 2), 2) + Math.cos(lastLatitude * Math.PI / 180) * Math.cos(currentLatitude * Math.PI / 180) * Math.pow(Math.sin((currentLongitude - lastLongitude) * Math.PI / 180 / 2), 2)));
+                        currentLocation.setDistance(distance);
+                        currentLocation.setTotalDistance(lastLocation.getTotalDistance() + distance);
                         devicePositionRepository.save(currentLocation);
                     } else {
                         LOG.info("no location was found in the last location");
@@ -133,7 +125,7 @@ public final class Receiver {
                 if(deviceService.updateLatestLocation(device.getImei(), currentLocation)){
                     LOG.info("processed deviceId:{}, totalDistance :{}, distance;{}, stopped:{}", currentLocation.getUniqueId(),
                             currentLocation.getTotalDistance(), currentLocation.getDistance(),
-                            currentLocation.isIsStopped());
+                            currentLocation.isStopped());
                 }
             }
         }
